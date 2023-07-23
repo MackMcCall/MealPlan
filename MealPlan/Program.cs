5eeef43ee7d7aacf03ab97f47bbd13837c01ed8c
Mack McCall <mackmc9924@gmail.com>
2023-07-23 15:25:13 -0600

    forgot to save before last commit

--- MealPlan/Program.cs
+++ MealPlan/Program.cs
@@ -6,7 +6,7 @@ namespace MealPlan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the very begginings of the Meal Planning/Recipe Storage app I'm working on.\nThis will be a very long time coming. So, new features will be getting added as I learn the skills necessary to implement them.\nThanks to all the wonderful wives, moms, and meal planners out there!\n");
+            //Welcome to the very begginings of the Meal Planning/Recipe Storage app I'm working on. This will be a very long time coming. So, new features will be getting added as I learn the skills necessary to implement them. Thanks to all the wonderful wives, moms, and meal planners out there!
 
             Recipe Lasagna = new Recipe("Lasagna", "Sauce", 60);
             Console.WriteLine(Lasagna.name + " " + Lasagna.ingredient + " " + Lasagna.minutes);
